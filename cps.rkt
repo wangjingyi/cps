ecf32e03e621602626e5bc09c14c84c7b7607870
Jingyi <jwang@verizon.com>
2017-07-06 16:13:52 -0500

    remove inserted text

--- cps.rkt
+++ cps.rkt
@@ -1,7 +1,6 @@
-;; The first three lines of this file were inserted by DrRacket. They record metadata
-;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-advanced-reader.ss" "lang")((modname cps) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #t #t none #f () #f)))
+
 ;few examples for CPS style programming in Dr Racket
+
 
 (define (e-length lst cc)
   (cond [(empty? lst) (cc 0)]
