9b1931b03077adc9b98c4ad4bb2967dac92c6ecf
wangjingyi <wang.lisp@gmail.com>
2018-02-02 22:09:36 -0600

    add another way to do

--- permutation-combination.rkt
+++ permutation-combination.rkt
@@ -1,4 +1,5 @@
 #lang racket
+(require racket/list)
 
 (define (permutation sofar remaining)
   (if (= (length remaining) 0)
@@ -28,6 +29,54 @@
                              nremaining)))))
 
 
-(permutation empty (list 'a 'b 'c))
-(combination empty (list 1 2 3))
+;another way to do 
 
+(define (add-each elem l)
+  (foldl (λ (e acc)
+             (cons (cons elem e) acc))
+         empty
+         l))
+
+(define (combinate l)
+  (if (empty? l)
+      (list empty)
+      (local [(define f (car l))
+              (define r (cdr l))
+              (define result (combinate r))]
+        (append result (add-each f result)))))
+
+(define (swap i j l)
+  (local [(define i-elem (list-ref l i))
+          (define j-elem (list-ref l j))]
+    (list-set (list-set l j i-elem)
+              i
+              j-elem)))
+
+(define (permute l)
+  (if (empty? l)
+      (list empty)
+      (local [(define len (length l))
+              (define (iter i result)
+                (if (= i len)
+                    result
+                    (local [(define swapped (swap 0 i l))
+                            (define f (car swapped))
+                            (define r (cdr swapped))
+                            (define i-result (add-each f (permute r)))]
+                      (iter (add1 i) (append result i-result)))))]
+        (iter 0 empty))))
+
+(define (permute-all l)
+  (local [(define len (length l))
+          (define (iter i)
+            (if (= i len)
+                (list empty)
+                (local [(define swapped (swap 0 i l))
+                        (define f (car swapped))
+                        (define r (cdr swapped))]
+                  (append (add-each f (permute-all r))
+                          (iter (add1 i))))))]
+    (iter 0)))
+
+
+(permute (list 1 2 3))
